308a62f97cb1822e5e71283154862da2de58e5aa
hapeace <74109414+hapeace@users.noreply.github.com>
2020-12-30 01:37:06 +0900

    코드업 기초 100제
    줄바꿈
    \n

---
+++ 1003.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout<<"Hello\nWorld\n";
+	return 0;
+}
