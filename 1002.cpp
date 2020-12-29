8b50cae6c8a264c18f374667bacf0aef68ec5614
hapeace <74109414+hapeace@users.noreply.github.com>
2020-12-30 00:56:49 +0900

    코드업 기초 100제
    기본 출력 문제

---
+++ 1002.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout<<"Hello World"<<"\n";
+	
+	
+	return 0;
+}
