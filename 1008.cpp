094ab2c788caaf8c09a95e39b0ddfc39f959906c
hapeace <74109414+hapeace@users.noreply.github.com>
2020-12-30 03:59:08 +0900

    코드업 기초 100제
    유니코드 출력
    printf사용
    출력값이 이상하게 나옴
    왜지

---
+++ 1008.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	printf("\u250C\u252C\u2510\n");
+	printf("\u251C\u253C\u2524\n");
+	printf("\u2514\u2534\u2518\n");
+	return 0;
+}
